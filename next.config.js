8898f1098103f2207324958ffef309cd97b986d7
mizphses <fuminori@mizphs.es>
2023-12-25 21:18:20 +0900

    いなにわうどん'sアドバイスを試す

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withMDX = require('@next/mdx')()
 
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
