a5e3f0ff49859275379e80fdc2fe98238f6e7c46
mizphses <fuminori@mizphs.es>
2023-12-25 21:29:39 +0900

    troubleshoot

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withMDX = require('@next/mdx')()
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   trailingSlash: true,
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
